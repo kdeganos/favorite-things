2d98eeb5430b83d7b3b10cc062673d0efd2888cc
Daniel Wulff and Kevin Deganos <daniel@>
2016-03-28 16:49:44 -0700

    created card deck and word play practice activities

--- js/word-play.js
+++ js/word-play.js
@@ -1,6 +1,8 @@
-
 jQuery(document).ready(function() {
 
+  function isBigEnough(word) {
+    return word.length >= 3;
+  }
   var sentence = [];
   var newSentence = [];
 
@@ -9,11 +11,14 @@ jQuery(document).ready(function() {
     //split sentence into an array
     sentence = $("#input").val().split(" ");
     //loop to create new array with 3 letter words or bigger
-    sentence.forEach(function(word) {
-      if (word.length >= 3) {
-        newSentence.push(word);
-      }
-    });
+    // sentence.forEach(function(word) {
+    //   if (word.length >= 3) {
+    //     newSentence.push(word);
+    //   }
+    // });
+    //filter array to words lpnger or equal to three letters
+    newSentence = sentence.filter(isBigEnough);
+
     //reverse the order of the array
     newSentence.reverse();
     //back into string and print
